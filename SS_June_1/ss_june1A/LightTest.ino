4fe2334b2e8484adeeddf817e8dc1177b8323fa6
jeanachesnik <chesj445@newschool.edu>
2017-06-19 21:17:06 -0400

    Add files via upload
    June troubleshooting -- first attempt only running street seat lights off of the sensor.

---
+++ SS_June_1/ss_june1A/LightTest.ino
@@ -0,0 +1,71 @@
+void lighTest() {
+  //paste your light sensr loop code here
+
+  /* Get a new sensor event */ 
+  sensors_event_t event;
+  tsl.getEvent(&event);
+
+  Serial.print(event.light);
+  Serial.print("\t");
+
+  if (lightReading == 3){
+    lightReading = 0;
+  }
+
+  Serial.print(lightReading);
+  Serial.print("\t");
+
+  lightVals[lightReading] = event.light;
+
+
+    for(int i = 0; i < 3; i++)
+{
+  Serial.print(lightVals[i]);
+  Serial.print(", ");
+}
+
+  if (lightReading > 2) {
+    lightReading = -1;
+  }
+
+//a bit of math...
+lightTotal = lightVals[0] + lightVals[1] + lightVals[2]; //add the 3 light values...
+lightAvg = lightTotal / 3; // divide them by 3
+Serial.print(" Avg: ");
+Serial.print(lightAvg);
+Serial.print(" "); 
+
+ 
+  /* Display the results (light is measured in lux) */
+  if (lightAvg < darkVal) // NIGHT TIME: replace with average values from your array 
+  {
+    nightCounter++;
+    Serial.print("Nighttime below: "); 
+    Serial.print(darkVal);
+    Serial.print(" ");
+    //Serial.print("nightCounter: "); //we're keeping a count of the loops to turn lights off after a while
+    //Serial.print(nightCounter);
+    Serial.print(" ");
+
+    unsetRelays();
+    Serial.println ("Light avg is below dark value"); 
+
+    //
+
+//    if (nightCounter == 1,3,5) {
+//    solarGrace();
+  }
+
+
+else
+
+{
+
+    setRelays();
+    Serial.println ("Light avg is above dark value"); 
+
+  }
+
+
+
+}
